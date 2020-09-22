70069ea633b342c08e1786be5bba5cd590168f51
mehakjit <mehakjitsingh@gmail.com>
2020-09-22 09:10:27 +0530

    branch comit

--- src/newemployee/Employee_Present_or_Absent.java
+++ src/newemployee/Employee_Present_or_Absent.java
@@ -3,8 +3,11 @@ package newemployee;
 public class Employee_Present_or_Absent {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random()*10)%2;
+		if (empCheck == FULL_TIME)
+			System.out.println("Emp is present");
+		else 
+			System.out.println("Emp is absent");
 	}
-
 }
